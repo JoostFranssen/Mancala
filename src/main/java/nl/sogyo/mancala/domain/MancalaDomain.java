a84ab1afd11fda3d2dd1e494f01ee778db303172
Joost Franssen <jfranssen@sogyo.nl>
2019-10-29 09:07:54 +0100

    Make the conversion between array and Mancala interface indices clearer; disallow blank player names and fix returning of winning player name

--- src/main/java/nl/sogyo/mancala/domain/MancalaDomain.java
+++ src/main/java/nl/sogyo/mancala/domain/MancalaDomain.java
@@ -13,6 +13,13 @@ public class MancalaDomain implements Mancala {
 		bowl = (new House(player).getNeighbor(9));
 	}
 	
+	public int mancalaIndexToArrayIndex(int mancalaIndex) {
+		return mancalaIndex - 1;
+	}
+	public int arrayIndexToMancalaIndex(int arrayIndex) {
+		return arrayIndex + 1;
+	}
+	
 	public void validatePlayerIndex(int playerIndex) throws IllegalStateException {
 		if(playerIndex != 1 && playerIndex != 2) {
 			throw new IllegalStateException("Invalid player index: " + playerIndex);
@@ -22,40 +29,46 @@ public class MancalaDomain implements Mancala {
 	@Override
 	public String getPlayerName(int playerIndex) throws IllegalStateException {
 		validatePlayerIndex(playerIndex);
-		return playerNames[playerIndex - 1];
+		return playerNames[mancalaIndexToArrayIndex(playerIndex)];
 	}
 
+	/**
+	 * @exception IllegalArgumentException if {@code name} is blank
+	 */
 	@Override
 	public void setPlayerName(String name, int playerIndex) throws IllegalStateException {
 		validatePlayerIndex(playerIndex);
-		playerNames[playerIndex - 1] = name;
+		if(name.isBlank()) {
+			throw new IllegalArgumentException("A player's name cannot be blank");
+		}
+		playerNames[mancalaIndexToArrayIndex(playerIndex)] = name;
 	}
 
 	@Override
 	public boolean isToMovePlayer(int playerIndex) throws IllegalStateException {
 		validatePlayerIndex(playerIndex);
-		return players[playerIndex - 1].hasTurn();
+		return players[mancalaIndexToArrayIndex(playerIndex)].hasTurn();
 	}
 
 	@Override
 	public int[] playRecess(int index) {
-		House house = (House)bowl.getNeighbor(index - 1);
+		House house = (House)bowl.getNeighbor(mancalaIndexToArrayIndex(index));
 		house.play(players[0].hasTurn() ? players[0] : players[1]);
 		return exportGameState();
 	}
 
 	@Override
 	public int getStonesForPit(int index) {
-		return bowl.getNeighbor(index - 1).getBeads();
+		return bowl.getNeighbor(mancalaIndexToArrayIndex(index)).getBeads();
 	}
 
 	@Override
 	public int[] exportGameState() {
 		int[] state = new int[15];
 		for(int i = 0; i < 14; i++) {
-			state[i] = getStonesForPit(i);
+			state[i] = getStonesForPit(arrayIndexToMancalaIndex(i));
 		}
-		state[14] = (players[0].hasTurn() ? 1 : 2);
+		state[14] = (players[mancalaIndexToArrayIndex(1)].hasTurn() ? 1 : 2);
 		return state;
 	}
 
@@ -64,8 +77,19 @@ public class MancalaDomain implements Mancala {
 		return players[0].gameHasEnded();
 	}
 
+	/**
+	 * @return {@inheritDoc} In case of a tie an empty string is returned.
+	 */
 	@Override
 	public String getWinnersName() {
-		return playerNames[bowl.getWinner() == players[0] ? 1: 2];
+		if(!isEndOfGame()) {
+			return null;
+		}
+		Player winner = bowl.getWinner();
+		if(winner == null) {
+			return "";
+		} else {
+			return playerNames[winner == players[0] ? 0 : 1];
+		}
 	}
 }
